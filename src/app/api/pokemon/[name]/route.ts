d9c05417668d680dd2474ba196995381f62cc001
6614421026 <6614421026@rbru.ac.th>
2026-03-10 12:39:43 +0700

    first commit

--- src/app/api/pokemon/[name]/route.ts
+++ src/app/api/pokemon/[name]/route.ts
@@ -1,36 +1,43 @@
+import { NextResponse } from "next/server";
+
+type Pokemon = {
+  id: number
+  name: string
+  image: string
+  type: string
+  height: number
+  weight: number
+}
+
 export async function GET(
   req: Request,
   context: { params: Promise<{ name: string }> }
 ) {
 
-  try {
+  const { name } = await context.params
 
-    const { name } = await context.params
+  const res = await fetch(
+    `https://pokeapi.co/api/v2/pokemon/${name}`
+  )
 
-    const res = await fetch(
-      `https://pokeapi.co/api/v2/pokemon/${name}`
-    )
-
-    const data = await res.json()
-
-    const pokemon = {
-      id: data.id,
-      name: data.name,
-      image: data.sprites.front_default,
-      type: data.types[0].type.name,
-      height: data.height,
-      weight: data.weight
-    }
-
-    return Response.json(pokemon)
-
-  } catch (error) {
-
-    return Response.json(
+  if (!res.ok) {
+    return NextResponse.json(
       { error: "Pokemon not found" },
       { status: 404 }
     )
-
   }
+
+  const data = await res.json()
+
+  const pokemon: Pokemon = {
+    id: data.id,
+    name: data.name,
+    image: data.sprites.front_default,
+    type: data.types[0].type.name,
+    height: data.height,
+    weight: data.weight
+  }
+
+  return NextResponse.json(pokemon)
 
 }
