8c778bf12ef15bba3fcc7951edcbe1ab1e1be1ca
6614421026 <6614421026@rbru.ac.th>
2026-03-12 14:44:38 +0700

    first commit

--- src/app/api/pokemon/[name]/route.ts
+++ src/app/api/pokemon/[name]/route.ts
@@ -13,31 +13,40 @@ export async function GET(
   req: Request,
   context: { params: Promise<{ name: string }> }
 ) {
+  try {
 
-  const { name } = await context.params
+    const { name } = await context.params
 
-  const res = await fetch(
-    `https://pokeapi.co/api/v2/pokemon/${name}`
-  )
-
-  if (!res.ok) {
-    return NextResponse.json(
-      { error: "Pokemon not found" },
-      { status: 404 }
+    const res = await fetch(
+      `https://pokeapi.co/api/v2/pokemon/${name}`
     )
+
+    if (!res.ok) {
+      return NextResponse.json(
+        { error: "Pokemon not found" },
+        { status: 404 }
+      )
+    }
+
+    const data = await res.json()
+
+    const pokemon: Pokemon = {
+      id: data.id,
+      name: data.name,
+      image: data.sprites.front_default,
+      type: data.types[0].type.name,
+      height: data.height,
+      weight: data.weight
+    }
+
+    return NextResponse.json(pokemon)
+
+  } catch (error) {
+
+    return NextResponse.json(
+      { error: "Internal Server Error" },
+      { status: 500 }
+    )
+
   }
-
-  const data = await res.json()
-
-  const pokemon: Pokemon = {
-    id: data.id,
-    name: data.name,
-    image: data.sprites.front_default,
-    type: data.types[0].type.name,
-    height: data.height,
-    weight: data.weight
-  }
-
-  return NextResponse.json(pokemon)
-
 }
